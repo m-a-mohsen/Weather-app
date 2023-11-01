05748b974297dcf190e1604256f365be27229226
syennings <syhennings@live.com>
2023-11-01 16:28:28 +0100

    anything

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,13 +1,15 @@
 /* eslint-disable react/prop-types */
 export function List({ list, onDeleteActivity }) {
-  // console.log(list)
   return (
-    <ul>
+    <ul className="list">
       {list.map((element) => (
         <>
           <li key={element.id}>
             {element.name}
-            <button onClick={() => onDeleteActivity(element.id)}>
+            <button
+              key={element.id}
+              onClick={() => onDeleteActivity(element.id)}
+            >
               x
             </button>
           </li>
